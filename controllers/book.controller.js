135cc4e933a7dc9206c6ef080d8ff46ffa39fe66
Aliaksandr Minchankou <aliaksandr.minchankou@kronos.com>
2019-05-04 11:38:08 +0300

    Delete unused class in book.controller

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -1,57 +1,6 @@
 const mongoose = require('mongoose');
 const Book = require('../models/book');
 
-class BookController {
-
-    static addBook(book) {
-        let books = this.books;
-        let newBook = null;
-        if (book.title) {
-            newBook = {
-                id: books.length,
-                title: book.title,
-                author: book.author,
-                page_count: book.page_count,
-                year: book.year
-            };
-
-            books.push(newBook);
-        }
-
-        return newBook;
-    }
-
-    static getBook(req) {
-        let books = this.books;
-        let requestedBook = null;
-        books.map((book, i, arr) => {
-            if (book.id === +req.params.id) {
-                requestedBook = book;
-                return;
-            }
-        });
-        return requestedBook;
-    }
-
-    static editBook(req) {
-        let books = this.books;
-        let editedBook = null;
-
-        books.map((book, i, arr) => {
-            if (book.id === +req.params.id) {
-                book.title = req.body.title;
-                book.author = req.body.author;
-                book.page_count = req.body.page_count;
-                book.year = req.body.year;
-                editedBook = book;
-            }
-        });
-
-        return editedBook;
-    }
-
-}
-
 exports.getAllBooks = (req, res, next) => {
     Book.find()
         .then(books => {
@@ -152,5 +101,3 @@ exports.searchBookByName = (req, res, next) => {
             res.status(500).json({error: err});
         });
 };
-
-// module.exports = BookController;
